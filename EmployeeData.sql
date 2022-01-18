980bea0180f46471edf585b700dda884f557808b
Arib Ahmed <40217122+AribAhmed@users.noreply.github.com>
2022-01-18 11:33:03 -0800

    Update EmployeeData.sql

--- EmployeeData.sql
+++ EmployeeData.sql
@@ -20,13 +20,13 @@ MODIFY COLUMN employee_phonenum VARCHAR(50);
 
 DELETE FROM employee;
 
-INSERT INTO employee VALUES(1, 'Anton Loeb', 'Director', 500000, 'antonloeb3@gmail.com', '408-603-81636');
-INSERT INTO employee VALUES(2, 'Matthew Aragaw', 'Head of Operations', 350000, 'maragaw24@gmail.com', '408-858-8728');
-INSERT INTO employee VALUES(3, 'Derek Nguyen', 'Head of Technology', 100000, 'dereksli2018@gmail.com', '408-624-0305');
+INSERT INTO employee VALUES(1, 'Anton Loeb', 'Director', 500000, 'antonloeb3@gmail.com', '408-111-1212');
+INSERT INTO employee VALUES(2, 'Matthew Aragaw', 'Head of Operations', 350000, 'maragaw24@gmail.com', '408-121-1232');
+INSERT INTO employee VALUES(3, 'Derek Nguyen', 'Head of Technology', 100000, 'dereksli2018@gmail.com', '408-121-1112');
 INSERT INTO employee VALUES(4, 'Arib Ahmed', 'Head of IT', 100000, 'airarib@gmail.com', '408-457-4666');
-INSERT INTO employee VALUES(5, 'Maher Osman', 'Head of Marketing', 100000, 'maherosman121@gmail.com', '408-650-4436');
-INSERT INTO employee VALUES(6, 'Sidharth Naik', 'Head of Technology', 100000, 'sidharthdnaik@gmail.com', '669-226-9943');
-INSERT INTO employee VALUES(7, 'Adithya Gutala', 'Head of Marketing', 100000, 'adithya.gutala@gmail.com', '408-839-7632');
-INSERT INTO employee VALUES(8, 'Daniel Shin', 'Head of Finance', 100000, 'bahda930@hotmail.co.kr', '408-483-5775');
+INSERT INTO employee VALUES(5, 'Maher Osman', 'Head of Marketing', 100000, 'maherosman121@gmail.com', '408-321-1212');
+INSERT INTO employee VALUES(6, 'Sidharth Naik', 'Head of Technology', 100000, 'sidharthdnaik@gmail.com', '669-121-1121');
+INSERT INTO employee VALUES(7, 'Adithya Gutala', 'Head of Marketing', 100000, 'adithya.gutala@gmail.com', '408-221-1212');
+INSERT INTO employee VALUES(8, 'Daniel Shin', 'Head of Finance', 100000, 'bahda930@hotmail.co.kr', '408-112-1211');
 
 SELECT * from employee;
